8749890bf709da988ee901ec815767d83709cd03
William Bainton <baintonw@gmail.com>
2019-05-03 22:21:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,34 @@
-# Write your code here.
+katz_deli = []
+
+def line(deliline)
+  if deliline.empty?
+    puts "The line is currently empty."
+  else
+    line = "The line is currently:"
+      deliline.each_with_index do |x, i|
+       line << " #{i+1}. #{x}"
+      end
+    puts line
+  end
+end
+
+def take_a_number(deliline, name)
+  deliline.push(name)
+  puts "Welcome, #{name}. You are number #{deliline.length} in line."
+end
+
+def now_serving(deliline)
+  if deliline.length == 0
+    puts "The line is empty"
+end
+
+# #def line(deliline, name)
+#   if deliline.length == 0
+#     "The line is currently empty."
+#   else
+#   deliline.push(name)
+#   deliline.each_with_index do |x, i|
+#     puts "The line is currently: #{i+1}. #{x}"
+#   end
+# end
+# end
